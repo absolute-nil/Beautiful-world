d7e47b6c58144df13f453f3efcbade0a67ccfa6f
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-08-15 18:44:58 +0530

    create image input field

---
+++ lib/widgets/image_input.dart
@@ -0,0 +1,55 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ImageInput extends StatefulWidget {
+  @override
+  _ImageInputState createState() => _ImageInputState();
+}
+
+class _ImageInputState extends State<ImageInput> {
+  File _storedImage;
+
+  Future<void> _takePicture() async {
+    final picker = ImagePicker();
+    final imageFile =
+        await picker.getImage(source: ImageSource.camera, maxWidth: 600);
+    setState(() {
+      _storedImage = File(imageFile.path);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          height: 100,
+          width: 200,
+          decoration: BoxDecoration(
+              border:
+                  Border.all(width: 1, color: Theme.of(context).accentColor)),
+          child: _storedImage != null
+              ? Image.file(
+                  _storedImage,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                )
+              : Center(child: Icon(Icons.image)),
+          alignment: Alignment.center,
+        ),
+        SizedBox(
+          width: 10,
+        ),
+        Expanded(
+            child: FlatButton.icon(
+          onPressed: _takePicture,
+          icon: Icon(Icons.camera),
+          label: Text("Take Picture"),
+          textColor: Theme.of(context).primaryColor,
+        )),
+      ],
+    );
+  }
+}
