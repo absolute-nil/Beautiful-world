c51aa2a07534b2eb4edc71c0214eb4592a7ea929
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-08-16 02:46:38 +0530

    hook place detail screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import './providers/beautiful_places.dart';
 import './screens/places_list_screen.dart';
 import './screens/add_place_screen.dart';
+import './screens/place_detail_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -20,7 +21,8 @@ class MyApp extends StatelessWidget {
         ),
         home: PlacesListScreen(),
         routes: {
-          AddPlaceScreen.route_name : (ctx) =>AddPlaceScreen()
+          AddPlaceScreen.route_name : (ctx) =>AddPlaceScreen(),
+          PlaceDetailScreen.route_name: (ctx) => PlaceDetailScreen()
         },
       ),
     );
