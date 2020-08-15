c68026c2179d3e81ad629e5f11f55b54e0300117
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-08-16 02:48:01 +0530

    convert coords to address

--- lib/helpers/location_helper.dart
+++ lib/helpers/location_helper.dart
@@ -1,8 +1,20 @@
-import '../config/api.dart';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+const GOOGLE_API_KEY = "AIzaSyDT-4UxxjAed9WE6LRd8wzrCjM0c2JyCx8";
 
 class LocationHelper {
+
+
   static String generateLocationPreviewImage(
       {double longitude, double latitude}) {
-    return 'https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$longitude&zoom=13&size=600x300&maptype=roadmap&markers=color:red%7Clabel:I%7C$latitude,$longitude&key=${API.google_api_key}';
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=&$latitude,$longitude&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$latitude,$longitude&key=$GOOGLE_API_KEY';
+  }
+
+  static Future<String> getPlaceAddress({double longitude, double latitude}) async{
+    final url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=$latitude,$longitude&key=$GOOGLE_API_KEY';
+    final response = await http.get(url);
+    return json.decode(response.body)['results'][0]['formatted_address'];
   }
 }
